8c8f2f77d320cc9a0b8bf092fb6127f3b7e428e8
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-01-08 09:11:01 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    int size = phone_number.length();
+    for(int i = 0 ; i <size-4;i++)
+        answer += "*";
+    answer += phone_number.substr(size-4,4);
+    return answer;
+}
