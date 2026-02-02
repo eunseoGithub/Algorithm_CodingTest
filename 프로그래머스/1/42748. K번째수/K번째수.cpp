dcc20d75500a925c3f984807b0474383d958676e
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-02-02 09:54:52 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(const auto& cmd : commands)
+    {
+        int i = cmd[0];
+        int j = cmd[1];
+        int k = cmd[2];
+        vector<int> tmp(array.begin() + (i-1),array.begin() + j);
+        sort(tmp.begin(),tmp.end());
+        answer.push_back(tmp[k-1]);
+        
+    }
+    return answer;
+}
