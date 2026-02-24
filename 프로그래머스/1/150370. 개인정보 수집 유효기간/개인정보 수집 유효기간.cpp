ee867b5d92afd8e52eb640adad6d78107b1b9fc3
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-02-24 09:47:12 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.05 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -1,46 +1,9 @@
 #include <string>
-#include <string.h>
 #include <vector>
-#include <map>
-#include <algorithm>
-#include <iostream>
-using namespace std;
 
-static int Todays(const string& date)
-{
-    int year = stoi(date.substr(0,4));
-    int month = stoi(date.substr(5,2));
-    int day = stoi(date.substr(8,2));
-    return (year* 12 * 28) + (month * 28) + day;
-}
+using namespace std;
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
     vector<int> answer;
-    map<string,int> termMap;
-    for(string term : terms)
-    {
-        string sub = term.substr(0,1);
-        int subMonth = stoi(term.substr(2));
-        termMap[sub] = subMonth;
-    }
-    int todayDays = Todays(today);
-    
-    int i = 0;
-    for(string date : privacies)
-    {
-        
-        int startDays = Todays(date.substr(0,10));
-        string term = date.substr(11,1);
-        
-        int months = termMap[term];
-        
-        if(todayDays >= startDays + months * 28)
-        {
-            answer.push_back(i +1);
-        }
-        i++;
-        
-    }
- 
     return answer;
 }
