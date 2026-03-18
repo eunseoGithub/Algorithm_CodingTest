8e26f3b927911207469b8536eb24fe0f46101883
eunseoGithub <75939305+eunseoGithub@users.noreply.github.com>
2026-03-18 10:57:50 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    stack<int> st;
+    string cmd;
+
+    while (N--)
+    {
+        cin >> cmd;
+
+        if (cmd == "push")
+        {
+            int x;
+            cin >> x;
+            st.push(x);
+        }
+        else if (cmd == "pop")
+        {
+            if (st.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << st.top() << '\n';
+                st.pop();
+            }
+        }
+        else if (cmd == "size")
+        {
+            cout << st.size() << '\n';
+        }
+        else if (cmd == "empty")
+        {
+            cout << (st.empty() ? 1 : 0) << '\n';
+        }
+        else if (cmd == "top")
+        {
+            if (st.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << st.top() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
